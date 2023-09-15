a681c63b3f30dc61f657d02066faa2d4411ec85e
Oksana_Rozbytska <rozbutska34@gmail.com>
2023-09-15 17:58:58 +0300

    Updates

---
+++ _nuxt/static/1694789930/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foffer-page\u002F_nuxt\u002Fstatic\u002F1694789930",layout:"default",error:a,state:{theme:"dark",countdown:180},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Foffer-page\u002F",assetsPath:"\u002Foffer-page\u002F_nuxt\u002F",cdnURL:a}}}}(null));
